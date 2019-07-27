da6f5c350c883513e6da2b5735c7786ace1610aa
libenike <liberovsky.nikita@mail.ru>
2019-07-27 16:14:02 +0300

    Add cd command to script path in each script.

--- nvim/unpack.sh
+++ nvim/unpack.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-user_path=$(realpath ~)
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $script_path
+
+user_path=$(realpath ~)
 file=$script_path/init.vim
 path=$user_path/.config/nvim
 mkdir --parents $path
--- unpack.sh
+++ unpack.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $script_path
 
 for module in $(ls -d $script_path/*/)
 do

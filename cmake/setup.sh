6c442aa51c2fa3ffff32d69a62d5de37c006bfaf
libenike <liberovsky.nikita@mail.ru>
2019-12-05 12:17:51 +0300

    Add bash script for running cmake projects.

---
+++ cmake/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+build_path=$script_path/build
+compile_commands_path=$build_path/compile_commands.json
+link_compile_commands_path=$script_path/compile_commands.json
+
+cmake -S . -B build \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    &&
+if test -e $link_compile_commands_path; then
+    rm $link_compile_commands_path
+fi \
+    &&
+if test -f $compile_commands_path; then
+    ln -s $compile_commands_path $link_compile_commands_path \
+fi \
+    &&
+cmake --build build

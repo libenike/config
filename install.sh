0c85b4d0c202807ec364b80ed3d69a82306d3dbc
libenike <liberovsky.nikita@mail.ru>
2020-03-04 11:46:44 +0300

    Install all packages in one apt-get install command.

--- install.sh
+++ install.sh
@@ -2,21 +2,13 @@
 
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | sudo apt-key add -
 sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
-sudo apt-get update 
-sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo add-apt-repository ppa:neovim-ppa/stable
 
 sudo apt-get update
 
-sudo apt-get install clang-tools-8
-sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-8 100
-sudo apt-get install build-essential
-sudo apt-get install tmux
-sudo apt-get install apt-transport-https ca-certificates gnupg software-properties-common wget
-sudo apt-get install cmake
-sudo apt-get install neovim
-sudo apt-get install python
-sudo apt-get install python-pip
+sudo apt-get -y install build-essential clang-tools-8 tmux apt-transport-https ca-certificates gnupg software-properties-common wget cmake neovim python python-pip python3 python3-pip
+
 pip install --user pynvim
-sudo apt-get install python3
-sudo apt-get install python3-pip
 pip3 install --user pynvim
+
+sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-8 100

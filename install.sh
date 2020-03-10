fd64b2b82234593d80af737988910ec0917ea1c3
libenike <liberovsky.nikita@mail.ru>
2020-03-10 14:54:33 +0300

    Add update alternatives for clang-format.

--- install.sh
+++ install.sh
@@ -6,9 +6,10 @@ add-apt-repository -y ppa:neovim-ppa/stable
 
 apt-get update
 
-apt-get -y install build-essential clang-tools-8 tmux apt-transport-https ca-certificates gnupg software-properties-common wget cmake neovim python python-pip python3 python3-pip
+apt-get -y install build-essential clang-tools-8 tmux apt-transport-https ca-certificates gnupg software-properties-common wget cmake neovim python python-pip python3 python3-pip clang-format-9
 
 pip install --user pynvim
 pip3 install --user pynvim
 
 update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-8 100
+update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-9 100

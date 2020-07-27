72a80c5c97036aabb7e02f22facb88436dddcf5f
libenike <liberovsky.nikita@mail.ru>
2020-07-27 14:24:24 +0300

    Add bashrc custom support.

--- bash/unpack.sh
+++ bash/unpack.sh
@@ -3,6 +3,11 @@ script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $script_path
 
 user_path=$(realpath ~)
+
 file=$script_path/bash_aliases
 path=$user_path/.bash_aliases
+ln -s -T $file $path
+
+file=$script_path/bashrc
+path=$user_path/.bashrc
 ln -s -T $file $path
